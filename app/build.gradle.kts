b5410d9fab3861729023efa206ba99a6139e1273
Joker.X <853230859@qq.com>
2025-03-17 10:29:48 +0800

    配置网络请求依赖且新增解析模型

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,17 @@ dependencies {
     // https://kotlinlang.org/docs/serialization.html
     implementation(libs.kotlinx.serialization.json)
 
+    // okhttp 网络框架
+    // https://github.com/square/okhttp
+    implementation(libs.okhttp)
+
+    // 类型安全网络框架
+    // https://github.com/square/retrofit
+    implementation(libs.retrofit)
+
+    // 让Retrofit支持Kotlinx Serialization
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
     //region 依赖注入
     //https://developer.android.google.cn/training/dependency-injection/hilt-android?hl=zh-cn
     ksp(libs.hilt.android.compiler)
